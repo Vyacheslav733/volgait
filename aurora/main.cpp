1417f3d9c69dbfb3b4c096c719a5bfa310595da7
Vyacheslav <vyacheslavivanov1234@mail.ru>
2025-11-19 15:12:20 +0400

    Тест

--- aurora/main.cpp
+++ aurora/main.cpp
@@ -1,12 +1,9 @@
 #include <flutter/flutter_aurora.h>
-#include <flutter/flutter_compatibility_qt.h>
+#include <flutter/flutter_compatibility_qt.h>  // <- Add for Qt
+#include "generated_plugin_registrant.h"
 
 int main(int argc, char* argv[]) {
-    aurora::Initialize(argc, argv);
-    
-    aurora::EnableQtCompatibility();
-    
-    aurora::Launch();
-    
-    return 0;
+    aurora::FlutterApp app(argc, argv);
+    aurora::EnableQtCompatibility();                              // Enable Qt
+    return app.exec();
 }
